6901fb3fbc171a343eeb5ed9ac7cbc23b539fdb3
John Weis <weis.john@gmail.com>
2014-05-22 13:56:59 -0400

    completely refactored implementation of EventEmitting sleepyhollow

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
 
+var EventEmitter = require('./EventEmitter.js');
+
 var stderr = require('system').stderr;
 var stdin = require('system').stdin;
 
-function write(obj) {
-    stderr.write(JSON.stringify(obj) + "\n");
-    if (obj.event !== "ack") read();
-}
+(function(window) {
+    "use strict";
 
-function read() {
-    var data = stdin.readLine();
-    try { data.split("\v").forEach(_read) } 
-    catch (e) { throw new Error(data); }
-}
+    function _sleepyhollow() {
 
-function _read(data) { return _readLine(JSON.parse(data)); }
-function _readLine(obj) {
-    if (obj.event !== "ack") write({ event: "ack", message: obj });
-    // TODO: invoke routes here
-    // if (obj.event)
-}
+        // the modified event-emitter bridge
+        var sleepyhollow = new EventEmitter();
+        _emit = sleepyhollow.emit;
+        sleepyhollow.emit = function(event, message) {
+            if (event !== "ack") write({ event: event, message : message });
+            _emit.apply(sleepyhollow, Array.prototype.slice.call(arguments, 0));
+        }
 
-setInterval(function() {
-    write({ "event" : "heartbeat" });
-}, 1e2);
+        // custom write <> read bridge
+        function write(obj) {
+            stderr.write(JSON.stringify(obj) + "\n");
+            if (obj.event !== "ack") read();
+        }
 
-var page = require('webpage').create();
-page.open("http://github.com", function(status) {
-    if (status == "success") {
-        write({ "event": "open" });
-        // page.render('github.jpg');
-        write({ "event": "render" });
+        function read() {
+            var data = stdin.readLine();
+            try { data.split("\v").forEach(_read); }
+            catch (e) { throw new Error(data); }
+        }
+
+        function _read(data) { return _readLine(JSON.parse(data)); }
+        function _readLine(obj) {
+            if (obj.event !== "ack") write({ event: "ack", message: obj });
+            _emit.apply(sleepyhollow, [obj.event, obj.message]);
+        }
+
+        // minimal writing to make sure the Node side doesn't have to buffer
+        setInterval(function() { write({ "event" : "syn" }); }, 1e2);
+
+        return sleepyhollow;
     }
-    phantom.exit();
-});
+
+    return exports = (module || {}).exports = _sleepyhollow;
+
+})(this);
