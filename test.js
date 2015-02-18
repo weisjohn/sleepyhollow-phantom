f1448e5e48a7d9346021382a42672d96b1267146
John Weis <weis.john@gmail.com>
2015-02-17 22:34:26 -0500

    adding huge buffer generation string

--- test.js
+++ test.js
@@ -2,12 +2,26 @@
 var sleepyhollow = require('./index');
 var mrhyde = sleepyhollow();
 
+// generate a large string of alphabetical characters
+var len = 1e4, mod = 26, pad = 65;
+var response = [];
+for (var i = 0; i < len; i++) {
+    var c = (i % mod) + pad;
+    response += String.fromCharCode(c);
+}
+
+// when receiving a page command, navigate, render
 mrhyde.on('page', function(url) {
     var page = require('webpage').create();
     page.open(url, function(status) {
+
         mrhyde.emit("open");
+        mrhyde.emit("output", response);
+
+        // test rendering
         page.render("render.png");
         mrhyde.emit("render");
+
         page.close();
     });
 });
