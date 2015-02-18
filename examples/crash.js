0daaa15430545e0c4a008aa74e2b09dd3ed0a985
John Weis <weis.john@gmail.com>
2015-02-18 02:01:07 -0500

    a basic corresponding file to the crash example

---
+++ examples/crash.js
@@ -0,0 +1,16 @@
+
+var sleepyhollow = require('../index.js');
+var mrhyde = sleepyhollow();
+
+mrhyde.on('render', function(url) {
+    var page = require('webpage').create();
+    page.open(url, function(status) {
+        page.render(url.replace(/[\/:]/g, "") + ".png");
+        mrhyde.emit("rendered");
+        page.close();
+    });
+});
+
+mrhyde.on('end', function() {
+    phantom.exit();
+});
